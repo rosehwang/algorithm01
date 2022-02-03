b6cadcbd5f7522a2ffe0d4557455e5d42eee8a22
rosehwang <39663910+rosehwang@users.noreply.github.com>
2022-02-03 23:28:24 +0900

    clone github

---
+++ src/programmers/Algorithm_02.java
@@ -0,0 +1,30 @@
+class Solution {
+    /*CASE_1*/
+    public int solution(String s) {
+        int answer = 0;
+
+        for(int i=1; i<=(s.length()/2)+1; i++){//s가 9자리면 5번째까지 진행함.
+            //문자열을 압축할수 있는 단어의 최대갯수 구하는것.
+            int result = getSplitedLength(s, i, 1).length();
+            answer = i==1 ? result : (answer>result?result:answer);             
+        }
+        return answer;
+    }
+    
+    // s => 받은 문자열, n => 인덱싱(?), repeat => 반복횟수 
+    public String getSplitedLength(String s, int n, int repeat){
+        if(s.length() < n) return s;
+        String result = "";
+        String preString = s.substring(0, n);
+        String postString = s.substring(n, s.length());
+
+        // 불일치 -> 현재까지 [반복횟수 + 반복문자] 조합
+        if(!postString.startsWith(preString)){
+            if(repeat ==1) return result += preString + getSplitedLength(postString, n, 1);
+            return result += Integer.toString(repeat) + preString + getSplitedLength(postString, n, 1);
+        }
+
+        return result += getSplitedLength(postString, n, repeat+1);//재귀함수 
+    }
+}
+    
---
+++ src/programmers/Algorithm_03.java
@@ -0,0 +1,51 @@
+import java.util.ArrayList;
+
+class Solution {
+    public int solution(String s) {
+        int answer = s.length();
+
+        int maxDivider = s.length() / 2;
+
+        for(int divider = 1; divider <= maxDivider; divider++) {
+            int currentLength = 0;
+            int start = 0, end = divider;
+            ArrayList<String> pieces = new ArrayList<>();
+            while(start < s.length()) {
+                if(s.length() < end) {
+                    end = s.length();
+                }
+                pieces.add(s.substring(start, end));
+                start += divider;
+                end += divider;
+            }
+            int duplicate = 1;
+            String before = "";
+            String next = "";
+            for(int i = 0; i < pieces.size(); i++) {
+                before = pieces.get(i);
+                if(i == pieces.size() - 1) {
+                    next = "";
+                } else {
+                    next = pieces.get(i + 1);
+                }
+                if(before.equals(next)) {
+                    duplicate++;
+                } else {
+                    String temp = "";
+                    if(duplicate == 1) {
+                        temp = before;
+                    } else {
+                        temp = String.valueOf(duplicate) + before;
+                        duplicate = 1;
+                    }
+                    currentLength += temp.length();
+                }
+            }
+
+            if(answer > currentLength) {
+                answer = currentLength;
+            }
+        }
+        return answer;
+    }
+}
